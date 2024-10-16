4e0422ecbbb4afabed40bb9a5764fa7965ca46f4
WaelEshak <eng.wael.eshak@gmail.com>
2024-10-16 22:21:42 +0300

    WaelWork

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Home from './pages/Home/Home'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter , Route, Routes } from 'react-router-dom'
 import Products from "./pages/Products/Products.jsx"
 import ProductDetails from './pages/ProductDetails/ProductDetails.jsx'
-import  Footer  from './componants/Footer/Footer.jsx'
-import  Navbar  from './componants/Navbar/Navbar.jsx'
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import './App.css';
+import  Footer  from './componants/Footer/Footer2.jsx'
+import  Navbar  from './componants/Navbar/Navbar2.jsx'
+// import { BrowserRouter } from "react-router-dom";
+// import './App.css';
 import ProductList from './Components/productList';
 import ProductDetailsPage from './Components/ProductDetailsPage';
 import CartList from './Components/CartList';
@@ -20,22 +20,20 @@ function App() {
   return (
     
     <BrowserRouter>
+          <Navbar />
       
-          {/* <Navbar /> */}
        <Routes>
           <Route path='/' Component={Home} />
           <Route path='/products/:categoryName' Component={Products} />
           <Route path='/products/:categoryName/productDetails/:id' Component={ProductDetails} />
        
-       
-  
         <Route path="/products/details/:id" element={<ProductDetailsPage />} />
         <Route path="/products/cart/:userId" element={<CartList />} />
         <Route path="/products" element={<ProductList />} />
         <Route path="/checkout/:userId/:productId/:quantity" element={<CheckOut />} />
         <Route path="/confirmation" element={<Confirmation />} />
       </Routes>
-       {/* <Footer /> */ }
+      <Footer />
     </BrowserRouter>
   )
 }
